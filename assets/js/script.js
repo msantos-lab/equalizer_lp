0ceb96838143dd245b45af79cc36e47f64245bff
msantos-lb <msantosdev.lab@gmail.com>
2023-03-28 22:33:44 +0100

    envio ficheiros

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const illustration = document.querySelector(".app"),
+	txt = document.querySelector(".text");
+
+window.addEventListener("scroll", () => {
+	const position_illu = illustration.getBoundingClientRect(),
+		position_txt = txt.getBoundingClientRect(),
+		windowHeight = window.innerHeight - 140;
+
+	if (position_illu.top <= window.innerHeight) {
+		illustration.classList.add("fadeInUp");
+	}
+
+	if (position_txt.top <= windowHeight) {
+		txt.classList.add("fadeInUp");
+	}
+});
